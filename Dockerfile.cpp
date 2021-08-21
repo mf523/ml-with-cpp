6e7b69493833e51346456de34368dfc3e5229340
mf523 <50037263+mf523@users.noreply.github.com>
2021-08-21 16:32:53 +0100

    Update Dockerfile.cpp

--- Dockerfile.cpp
+++ Dockerfile.cpp
@@ -2,7 +2,7 @@ FROM lmiao/dev-env:ubuntu-base
 
 RUN apt-get -y dist-upgrade && \
     apt-get -y update && \
-    apt-get -y install git g++
+    apt-get -y install git g++ wget
 
-USER docker
+# USER docker
 CMD /bin/bash
